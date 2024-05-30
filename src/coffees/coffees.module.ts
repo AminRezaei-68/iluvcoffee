11c783caf6f837dac74f53b62f13069ba5398f31
AminRezaei <rezaei.amin1268@gmail.com>
2024-05-30 22:04:18 +0330

    add TRANSIENT scope to custom provider and edit useFactory such as course

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Injectable, Module } from '@nestjs/common';
+import { Injectable, Module, Scope } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -16,14 +16,20 @@ import { Connection, DataSource } from 'typeorm';
     CoffeesService,
     {
       provide: COFFEE_BRANDS,
-      useFactory: async (connection: DataSource): Promise<string[]> => {
-        //DataSouce Connection
-        const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe']);
-        console.log('Async Factory');
-        return coffeeBrands;
-      },
-      inject: [DataSource],
+      useFactory: () => ['buddy brew', 'nescafe'],
+      scope: Scope.TRANSIENT,
     },
+    // {
+    //   provide: COFFEE_BRANDS,
+    //   useFactory: async (connection: DataSource): Promise<string[]> => {
+    //     //DataSouce Connection
+    //     const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe']);
+    //     console.log('Async Factory');
+    //     return coffeeBrands;
+    //   },
+    //   scope: Scope.TRANSIENT,
+    //   inject: [DataSource],
+    // },
   ],
   exports: [CoffeesService],
 })
