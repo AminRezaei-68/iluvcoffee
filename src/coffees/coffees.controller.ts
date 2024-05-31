345a4039002bb9958b120bfcd66073d8d1df7503
AminRezaei <rezaei.amin1268@gmail.com>
2024-05-31 14:50:02 +0330

    inject the original request object in coffees controller

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -5,6 +5,7 @@ import {
   Body,
   Controller,
   Delete,
+  Inject,
   Param,
   Patch,
   Post,
@@ -15,10 +16,15 @@ import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/create-coffee.dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
+import { REQUEST } from '@nestjs/core';
+import { Request } from 'express';
 
 @Controller('coffees')
 export class CoffeesController {
-  constructor(private readonly coffeesService: CoffeesService) {
+  constructor(
+    private readonly coffeesService: CoffeesService,
+    @Inject(REQUEST) private readonly request: Request,
+  ) {
     console.log('coffees controller created.');
   }
 
