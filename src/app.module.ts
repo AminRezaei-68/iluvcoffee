91038df3692054e29f5fac9c70ddeafed9503214
AminRezaei <rezaei.amin1268@gmail.com>
2024-06-05 17:04:48 +0330

    add joi package and add schema validation to ConfigModule. forRoot

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,12 +10,15 @@ import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { CoffeeRatingService } from './coffee-rating/coffee-rating.service';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import Joi from '@hapi/joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.environment',
-      // ignoreEnvFile: true,
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+      }),
     }),
     CoffeesModule,
     TypeOrmModule.forRoot({
