9e3da6510a9b40e6c5ed1d878df1a11909a03331
AminRezaei <rezaei.amin1268@gmail.com>
2024-07-05 13:44:30 +0330

    add catchError callback function to our pipe to create user friendly error message

--- src/common/interceptors/timeout/timeout.interceptor.ts
+++ src/common/interceptors/timeout/timeout.interceptor.ts
@@ -4,12 +4,21 @@ import {
   ExecutionContext,
   Injectable,
   NestInterceptor,
+  RequestTimeoutException,
 } from '@nestjs/common';
-import { Observable, timeout } from 'rxjs';
+import { Observable, TimeoutError, throwError } from 'rxjs';
+import { timeout, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class TimeoutInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle().pipe(timeout(3000));
+    return next.handle().pipe(
+      timeout(3000),
+      catchError((error) => {
+        if (error instanceof TimeoutError) {
+          return throwError(new RequestTimeoutException());
+        }
+      }),
+    );
   }
 }
