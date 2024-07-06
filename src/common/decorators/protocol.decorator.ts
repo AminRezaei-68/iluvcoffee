a682b2efe238cf1f8faee393802c854aa44a0e0a
AminRezaei <rezaei.amin1268@gmail.com>
2024-07-06 21:36:57 +0330

    create protocol decorator for creating our owen custom decerator

---
+++ src/common/decorators/protocol.decorator.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const Protocol = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.Protocol;
+  },
+);
