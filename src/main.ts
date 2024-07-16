268ce87c85f9086a35877ec16f6083fe0de3f740
AminRezaei <rezaei.amin1268@gmail.com>
2024-07-16 18:10:25 +0330

    add swagger module and setting up the swagger by generating a base document

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ApiKeyGuard } from './common/guards/api-key/api-key.guard';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
 import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -25,6 +26,13 @@ async function bootstrap() {
     new WrapResponseInterceptor(),
     new TimeoutInterceptor(),
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('I love Coffee')
+    .setDescription('Coffee aplication')
+    .setVersion('1.0')
+    .build();
+
   await app.listen(3000);
 }
 bootstrap();
