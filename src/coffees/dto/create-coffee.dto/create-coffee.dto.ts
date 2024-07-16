7755bf8a57462aee31da7cff45e421670e7cf1c4
AminRezaei <rezaei.amin1268@gmail.com>
2024-07-16 21:32:49 +0330

    annotate with new decorator @ApiProperty()

--- src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
@@ -1,13 +1,17 @@
 /* eslint-disable prettier/prettier */
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateCoffeeDto {
+  @ApiProperty()
   @IsString()
   readonly name: string;
 
+  @ApiProperty()
   @IsString()
   readonly brand: string;
 
+  @ApiProperty()
   @IsString({ each: true })
   readonly flavors: string[];
 }
