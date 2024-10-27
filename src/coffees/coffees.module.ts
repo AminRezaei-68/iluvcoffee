8f0a4b581e88b0ea20421ff0d671708396969a7d
AminRezaei <rezaei.amin1268@gmail.com>
2024-10-27 17:35:22 +0330

    add Event to MongooseModule in coffees.module file

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -4,10 +4,17 @@ import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Coffee, CoffeeSchema } from './entities/coffee.entity';
+import {
+  Event,
+  EventSchema,
+} from 'src/events/entities/event.entity/event.entity';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Coffee.name, schema: CoffeeSchema }]),
+    MongooseModule.forFeature([
+      { name: Coffee.name, schema: CoffeeSchema },
+      { name: Event.name, schema: EventSchema },
+    ]),
   ],
   controllers: [CoffeesController],
   providers: [CoffeesService],
