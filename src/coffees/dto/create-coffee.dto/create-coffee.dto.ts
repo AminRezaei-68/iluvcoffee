1dced56e5467141253fd63f04fc1b6748bee9a14
AminRezaei <rezaei.amin1268@gmail.com>
2024-10-27 12:19:16 +0330

    Commenting out the id parameter in the DTO - We need the id parameter because we are using Mongo

--- src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
@@ -2,8 +2,8 @@
 import { IsString } from 'class-validator';
 
 export class CreateCoffeeDto {
-  @IsString()
-  readonly id: number;
+  // @IsString()
+  // readonly id: number;
 
   @IsString()
   readonly name: string;
